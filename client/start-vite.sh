fdac2024c33a1a773449e1b3ca01bdfd7a9bab77
gustavogds <gustavogds142000@gmail.com>
2026-04-27 11:35:00 -0300

    Fix port auth issue

--- client/start-vite.sh
+++ client/start-vite.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-exec npm run dev -- --clearScreen false --logLevel warn
+exec /usr/bin/npm run dev -- --host 127.0.0.1 --port 5175 --clearScreen false --logLevel warn
